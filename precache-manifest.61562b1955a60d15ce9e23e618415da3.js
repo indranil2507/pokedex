c14f97a143b6c0e7a8365c887b5ec5a43be6ab3b
indranil2507 <iindranilsaha1@gmail.com>
2020-08-10 10:06:21 +0530

    Updates

---
+++ precache-manifest.61562b1955a60d15ce9e23e618415da3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922b47d33d92d35076f63cba2b96edcb",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "53be5e409f4dfba926f3",
+    "url": "/pokedex/static/css/main.2e6a2d9c.chunk.css"
+  },
+  {
+    "revision": "88b3df32e39bcad5b317",
+    "url": "/pokedex/static/js/2.24e91571.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokedex/static/js/2.24e91571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53be5e409f4dfba926f3",
+    "url": "/pokedex/static/js/main.4434244d.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
